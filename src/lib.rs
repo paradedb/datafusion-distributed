0701cd4322a4f67304982bd7ca64960cae11937b
Stu Hood <stuhood@gmail.com>
2026-08-19 13:59:14 -0700

    Switch to using the upstream `impl WorkerChannel for InProcessWorkerClient`, and remove self-loop behavior in the `shm` implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -74,9 +74,9 @@ pub use worker_resolver::{WorkerResolver, get_distributed_worker_resolver};
 
 pub use protocol::{
     ChannelResolver, CoordinatorToWorkerMsg, ExecuteTaskRequest, GetWorkerInfoRequest,
-    GetWorkerInfoResponse, InProcessChannelResolver, LoadInfo, ProducedDynamicFilter,
-    SetPlanRequest, TaskCompletedDynamicFilters, TaskDynamicFilter, TaskKey, TaskMetrics,
-    WorkUnitBatch, WorkUnitFeedDeclaration, WorkUnitMsg, WorkerChannel, WorkerToCoordinatorMsg,
+    GetWorkerInfoResponse, LoadInfo, ProducedDynamicFilter, SetPlanRequest,
+    TaskCompletedDynamicFilters, TaskDynamicFilter, TaskKey, TaskMetrics, WorkUnitBatch,
+    WorkUnitFeedDeclaration, WorkUnitMsg, WorkerChannel, WorkerToCoordinatorMsg,
     decode_task_metrics, get_distributed_channel_resolver,
 };
 pub use stage::{
